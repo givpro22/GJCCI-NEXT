928b4cf480a570ce4d75173031a58fe8645ac8df
박영서 <givpro22@daum.net>
2025-11-21 01:34:47 +0900

    chore: auth.config 설정

---
+++ auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { z } from "zod";
+import type { User } from "@/lib/definitions";
+import { createClient } from "./utils/supabase/server";
+// import bcrypt from "bcrypt";
+
+const supabase = await createClient();
+
+export async function getUser(email: string): Promise<User | null> {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email)
+    .maybeSingle<User>(); // 또는 .single<User>()
+
+  if (error) {
+    console.error("getUser error", error);
+    return null;
+  }
+
+  return data;
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
